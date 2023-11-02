0648a61657b88e50a8b96d6eb01fc55cd0c41d95
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-11-02 14:24:41 +0100

    added about us component

---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function AboutUs() {
+    return (
+        <div className='about-us'>
+            <h1>About us</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum assumenda tenetur nam eius corrupti odit odio, voluptatem officiis. Dolorem vero, obcaecati ad asperiores enim a nesciunt nihil deserunt. Beatae, deserunt.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis voluptatum commodi fugiat tempore vero, eum natus tempora eius iste, tenetur, ducimus beatae aut nisi delectus minima dolorem distinctio nobis consequatur.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum rem ex pariatur ratione voluptates nam officia cupiditate nulla. Quam dicta vero architecto quisquam, ducimus vel aliquid deserunt nemo assumenda eligendi.
+            </p>
+        </div>
+    );
+}
