2a35d45a56bf29841e6c3b4bb78870521b1082d2
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-11-02 14:50:00 +0100

    added privacy policy component

---
+++ src/components/PrivacyPolicy.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function PrivacyPolicy() {
+    return(
+        <div className='privacy-policy'>
+            <h1>1.Privacy Policy</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita animi vero! Quos provident enim vero? Corrupti deserunt molestiae, accusantium odit repudiandae ullam molestias nemo! Rerum nam quaerat exercitationem nemo.</p>
+            <h2>2.Introduction</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita animi vero! Quos provident enim vero? Corrupti deserunt molestiae, accusantium odit repudiandae ullam molestias nemo! Rerum nam quaerat exercitationem nemo.</p>
+            <h2>3.Types of data collected</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita animi vero! Quos provident enim vero? Corrupti deserunt molestiae, accusantium odit repudiandae ullam molestias nemo! Rerum nam quaerat exercitationem nemo.</p>
+            <h2>4.Purpose of data collection</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita animi vero! Quos provident enim vero? Corrupti deserunt molestiae, accusantium odit repudiandae ullam molestias nemo! Rerum nam quaerat exercitationem nemo.</p>
+            <h2>5.Legal basis</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita animi vero! Quos provident enim vero? Corrupti deserunt molestiae, accusantium odit repudiandae ullam molestias nemo! Rerum nam quaerat exercitationem nemo.</p>
+            <h2>6.Data collection methods</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita animi vero! Quos provident enim vero? Corrupti deserunt molestiae, accusantium odit repudiandae ullam molestias nemo! Rerum nam quaerat exercitationem nemo.</p>
+            <h2>7.Data storage</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita animi vero! Quos provident enim vero? Corrupti deserunt molestiae, accusantium odit repudiandae ullam molestias nemo! Rerum nam quaerat exercitationem nemo.</p>
+            <h2>8.Data sharing</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita animi vero! Quos provident enim vero? Corrupti deserunt molestiae, accusantium odit repudiandae ullam molestias nemo! Rerum nam quaerat exercitationem nemo.</p>
+            <h2>9.User rights</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita animi vero! Quos provident enim vero? Corrupti deserunt molestiae, accusantium odit repudiandae ullam molestias nemo! Rerum nam quaerat exercitationem nemo.</p>
+            <h2>10.Data security</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita animi vero! Quos provident enim vero? Corrupti deserunt molestiae, accusantium odit repudiandae ullam molestias nemo! Rerum nam quaerat exercitationem nemo.</p>
+            <h2>11.Cookies and tracking technologies</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita animi vero! Quos provident enim vero? Corrupti deserunt molestiae, accusantium odit repudiandae ullam molestias nemo! Rerum nam quaerat exercitationem nemo.</p>
+            <h2>12.Contact</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita animi vero! Quos provident enim vero? Corrupti deserunt molestiae, accusantium odit repudiandae ullam molestias nemo! Rerum nam quaerat exercitationem nemo.</p>
+            <h2>13.Changes to this privacy policy</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita animi vero! Quos provident enim vero? Corrupti deserunt molestiae, accusantium odit repudiandae ullam molestias nemo! Rerum nam quaerat exercitationem nemo.</p>
+            <h2>14.Complaints and reports of violations</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita animi vero! Quos provident enim vero? Corrupti deserunt molestiae, accusantium odit repudiandae ullam molestias nemo! Rerum nam quaerat exercitationem nemo.</p>
+        </div>
+    );
+}
