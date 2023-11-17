7b7beaabc798dd1b2af7e6f6fb1399a0418f7556
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-11-17 15:37:38 +0100

    added location tracking

--- src/components/LocationInput.tsx
+++ src/components/LocationInput.tsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-const LocationInput: React.FC = () => {
+export default function LocationInput() {
   const [position, setPosition] = useState<[number, number] | null>(null);
   const [zoom, setZoom] = useState<number>(13);
+  const mapRef = React.useRef<any>();
 
   const handleMapClick = (e: any) => {
     const { lat, lng } = e.latlng;
@@ -19,9 +20,29 @@ const LocationInput: React.FC = () => {
     return null;
   };
 
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (location) => {
+          const { latitude, longitude } = location.coords;
+          setPosition([latitude, longitude]);
+          setZoom(15);
+          if (mapRef.current) {
+            mapRef.current.flyTo([latitude, longitude], 15);
+          }
+        },
+        (error) => {
+          console.error("Error getting user location:", error.message);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  };
+
   return (
     <div className="location-input">
-      <MapContainer center={[51.505, -0.09]} zoom={zoom} style={{ height: "400px", width: "100%" }}>
+      <MapContainer ref={mapRef} center={[51.505, -0.09]} zoom={zoom} style={{ height: "400px", width: "100%" }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -34,8 +55,7 @@ const LocationInput: React.FC = () => {
         )}
       </MapContainer>
       <p>{position}</p>
+      <button onClick={getUserLocation}>Get My Location</button>
     </div>
   );
 };
-
-export default LocationInput;
