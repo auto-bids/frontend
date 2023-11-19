416c952fff25ea54ab717286adfb090665f0b4c7
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-11-19 13:53:32 +0100

    changed offer page to take external data

--- src/components/OfferPage.tsx
+++ src/components/OfferPage.tsx
@@ -1,13 +1,67 @@
 import React from "react";
 import BidElement from "./BidElement";
 import SellerElement from "./SellerElement";
+import { useEffect, useState } from "react";
+
+interface IOffer {
+    photos: string[];
+    title: string;
+    price: number;
+    make: string;
+    model: string;
+    type: string;
+    year: number;
+    mileage: number;
+    engineCapacity: number;
+    fuel: string;
+    power: number;
+    transmission: string;
+    drive: string;
+    steering: string;
+    color: string;
+    doors: number;
+    seats: number;
+    registrationNumber: string;
+    firstRegistration: number;
+    condition: string;
+    vinNumber: string;
+    description: string;
+    features: string[];
+    seller: {
+        name: string;
+        phone: string;
+        email: string;
+        x: number;
+        y: number;
+    };
+};
 
 export default function OfferPage(){
+
+    const [offerData, setOfferData] = useState<IOffer | null>(null);
+
+    // useEffect(() => {
+    //     fetch("https://essa.com/api/offer/123")
+    //     .then((response) => response.json())
+    //     .then((data) => setOfferData(data))
+    //     .catch((error) => console.error("Error fetching data:", error));
+    // }, []);
+
+    //code below is for testing purposes only, it will be replaced with the code above
+    useEffect(() => {
+        import("../testJsons/testOffer.json")
+          .then((data) => setOfferData(data.default))
+          .catch((error) => console.error("Error loading local data:", error));
+      }, []);
+
+    if (!offerData) {
+        return <p>Loading...</p>;
+    }
     return(
         <div className="offer-page">
             <div className="offer-page-top-bar">
-                <h1>Offer title</h1>
-                <h1>Price</h1>
+                <h1>{offerData.title}</h1>
+                <p>{offerData.price}</p>
                 <button className="offer-page-top-bar-button">Add to favourites</button>
             </div>
             <div className="offer-page-main">
@@ -16,51 +70,64 @@ export default function OfferPage(){
                         <img src="/test.jpeg" alt="offer" />
                     </div>
                     <div className="offer-images-other-images">
-                        <img src="/test.jpeg" alt="offer" />
-                        <img src="/test.jpeg" alt="offer" />
-                        <img src="/test.jpeg" alt="offer" />
-                        <img src="/test.jpeg" alt="offer" />
-                        <img src="/test.jpeg" alt="offer" />
+                        {offerData.photos.map((photo) => (
+                            <img src={photo} alt="offer" />
+                        ))}
                     </div>
                 </div>
                 <div className="offer-page-main-details">
-                    <p>Make:</p>
-                    <p>Model:</p>
-                    <p>Type:</p>
-                    <p>Year:</p>
-                    <p>Mileage:</p>
-                    <p>Engine capacity:</p>
-                    <p>Fuel:</p>
-                    <p>Power:</p>
-                    <p>Transmission:</p>
-                    <p>Drive:</p>
-                    <p>Steering:</p>
-                    <p>Color:</p>
-                    <p>Doors:</p>
-                    <p>Seats:</p>
-                    <p>Registration number:</p>
-                    <p>First registration:</p>
-                    <p>Condition:</p>
-                    <p>Vin number:</p>
+                    <label>Make:</label>
+                    <p>{offerData.make}</p>
+                    <label>Model:</label>
+                    <p>{offerData.model}</p>
+                    <label>Type:</label>
+                    <p>{offerData.type}</p>
+                    <label>Year:</label>
+                    <p>{offerData.year}</p>
+                    <label>Mileage:</label>
+                    <p>{offerData.mileage}</p>
+                    <label>Engine Capacity:</label>
+                    <p>{offerData.engineCapacity}</p>
+                    <label>Fuel:</label>
+                    <p>{offerData.fuel}</p>
+                    <label>Power:</label>
+                    <p>{offerData.power}</p>
+                    <label>Transmission:</label>
+                    <p>{offerData.transmission}</p>
+                    <label>Drive:</label>
+                    <p>{offerData.drive}</p>
+                    <label>Steering:</label>
+                    <p>{offerData.steering}</p>
+                    <label>Color:</label>
+                    <p>{offerData.color}</p>
+                    <label>Doors:</label>
+                    <p>{offerData.doors}</p>
+                    <label>Seats:</label>
+                    <p>{offerData.seats}</p>
+                    <label>Registration Number:</label>
+                    <p>{offerData.registrationNumber}</p>
+                    <label>First Registration:</label>
+                    <p>{offerData.firstRegistration}</p>
+                    <label>Condition:</label>
+                    <p>{offerData.condition}</p>
+                    <label>VIN Number:</label>
+                    <p>{offerData.vinNumber}</p>
                 </div>
                 <div className="offer-page-main-bid">
                     <BidElement/>
                 </div>
                 <div className="offer-page-main-description">
                     <h2>Description</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis nisl euismod, aliquet magna sed, ultrices urna. Sed sit amet mauris ut nulla bibendum rutrum. Sed euismod, dui eget aliquam aliquet, tellus elit suscipit risus, a lacinia nunc nibh vitae diam. Donec vel quam vel nunc aliquet tempor. Donec non semper elit. Duis et nunc ut leo ultrices pellentesque. Nulla facilisi. Nulla facilisi. Donec ut erat in nisl aliquam ultricies. Etiam ultricies, elit quis gravida ultricies, nunc leo eleifend libero, ut laoreet eros nunc a nunc. Nulla facilisi. Sed eget nisl nec nisi aliquam aliquet. Nullam euismod, felis nec consectetur gravida, nisl nibh varius felis, in pellentesque ex ipsum non mi. Donec euismod, ligula at luctus tristique, nunc diam aliquam magna, vel pulvinar libero leo in ante. Cras at semper nunc.</p>
+                    <p>{offerData.description}</p>
                 </div>
                 <div className="offer-page-main-features">
                     <h2>Features</h2>
-                    <p>Heated Seats</p>
-                    <p>Heated Mirrors</p>
-                    <p>Power Steering</p>
-                    <p>Power Windows</p>
-                    <p>AC</p>
-                    <p>Central Locking</p>
+                    {offerData.features.map((feature, index) => (
+                    <p key={index}>{feature}</p>
+                    ))}
                 </div>
                 <div className="offer-page-main-seller">
-                    <SellerElement/>
+                    <SellerElement name={offerData.seller.name} phone={offerData.seller.phone} email={offerData.seller.email} x={offerData.seller.x} y={offerData.seller.y}/>
                 </div>
             </div>
         </div>
--- src/components/SellerElement.tsx
+++ src/components/SellerElement.tsx
@@ -1,41 +1,26 @@
 import React, { useEffect } from "react";
 import MapComponent from "./MapComponent";
-declare global {
-  interface Window {
-    google: any;
-  }
+
+interface SellerElementProps {
+  name: string;
+  phone: string;
+  email: string;
+  x: number;
+  y: number;
 }
 
-export default function SellerElement() {
-  useEffect(() => {
-    if (window.google) {
-      const map = new window.google.maps.Map(document.getElementById("map"), {
-        center: { lat: 52.1138226, lng: 16.9311019 },
-        zoom: 10,
-      });
-
-      new window.google.maps.Marker({
-        position: { lat: 52.1138226, lng: 16.9311019 },
-        map: map,
-        title: "Seller Location",
-      });
-    }
-  }, []);
-
+export default function SellerElement({ name, phone, email, x, y }: SellerElementProps) {
   return (
     <div className="seller-element">
       <h1>Seller info</h1>
       <div className="seller-element-info">
-        <h2>Username</h2>
-        <p>Phone number</p>
-        <p>Email</p>
-        <p>City</p>
-        <p>Street</p>
-        <p>Post code</p>
+        <p>{name}</p>
+        <p>{phone}</p>
+        <p>{email}</p>
       </div>
       <div id="map" style={{ height: "400px", width: "100%" }}></div>
       <div className="seller-test-map">
-        <MapComponent x={52.114187} y={16.93129} />
+        <MapComponent x={x} y={y} />
       </div>
     </div>
   );
