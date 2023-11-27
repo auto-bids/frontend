c7e9c019768d7af5333d5c3cb5ca0cea1be1bf86
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-11-27 11:19:17 +0100

    updated makes lists for agricultural and construction machinery

--- tools/scraping.py
+++ tools/scraping.py
@@ -2,19 +2,16 @@ from bs4 import BeautifulSoup
 import re
 import json
 
-with open('trailersData.html') as html_file:
+with open('data.html') as html_file:
     html_data = html_file.read()
 
 
 soup = BeautifulSoup(html_data, 'html.parser')
 
-# Znajdź wszystkie elementy <span> z klasą 'ooa-micpln er34gjf0' wewnątrz <li>
 brand_elements = soup.select('li.ooa-t0lbh8 span.ooa-micpln.er34gjf0')
 
-# Wydziel nazwy marek z tagu <span>
 brands_list = [element.text.strip() for element in brand_elements]
 
-#omiń 2 pierwsze elementy
 brands_list = brands_list[2:]
 
 def remove_numbers_and_parentheses(brand):
@@ -22,6 +19,6 @@ def remove_numbers_and_parentheses(brand):
 
 cleaned_brands_list = [remove_numbers_and_parentheses(brand) for brand in brands_list]
 
-with open('trailersBrands.json', 'w', encoding='utf-8') as file:
+with open('brands.json', 'w', encoding='utf-8') as file:
     json.dump(cleaned_brands_list, file, ensure_ascii=False, indent=2)
 
