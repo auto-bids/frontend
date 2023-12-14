36298a18f7c6c25945cc359358d9593e43d56331
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-12-14 22:39:32 +0100

    fixed offers on search page

--- src/components/SearchPage.tsx
+++ src/components/SearchPage.tsx
@@ -4,122 +4,36 @@ import OfferElement from "./OfferElement";
 import { useEffect, useState } from "react";
 
 interface IOffer {
-    agriculturalMachinery: {
-        photos: string[];
-        title: string;
-        price: number;
-        year: number;
-    };
-    generalOffer: {
-        photos: string[];
-        title: string;
-        price: number;
-        year: number;
-    };
-    constructionMachinery: {
-        photos: string[];
-        title: string;
-        price: number;
-        year: number;
-    };
-    deliveryVans: {
-        photos: string[];
-        title: string;
-        price: number;
-        year: number;
-    };
-    motorcycles: {
-        photos: string[];
-        title: string;
-        price: number;
-        year: number;
-    };
-    trailers: {
-        photos: string[];
-        title: string;
-        price: number;
-        year: number;
-    };
-    trucks: {
-        photos: string[];
-        title: string;
-        price: number;
-        year: number;
-    };
+  id: string;
+  image: string;
+  title: string;
+  price: number;
+  year: number;
 };
 
 
 export default function SearchPage() {
 
     //just for testing
-    const [offerData, setOfferData] = useState<IOffer | null>(null);
+    const [offerData, setOfferData] = useState<IOffer[] | null>(null);
     useEffect(() => {
-        Promise.all([
-          import("../testJsons/testOfferAgriculturalMachinery.json"),
-          import("../testJsons/testOffer.json"),
-          import("../testJsons/testOfferConstructionMachinery.json"),
-          import("../testJsons/testOfferDeliveryVans.json"),
-          import("../testJsons/testOfferMotorcycles.json"),
-          import("../testJsons/testOfferTrailers.json"),
-          import("../testJsons/testOfferTrucks.json"),
-        ])
-          .then((data) => {
-            const [
-              agriculturalMachineryData,
-              offerData,
-              constructionMachineryData,
-              deliveryVansData,
-              motorcyclesData,
-              trailersData,
-              trucksData,
-            ] = data;
-      
-            setOfferData({
-              agriculturalMachinery: {
-                photos: agriculturalMachineryData.default.photos,
-                title: agriculturalMachineryData.default.title,
-                price: agriculturalMachineryData.default.price,
-                year: agriculturalMachineryData.default.year,
-              },
-              generalOffer: {
-                photos: offerData.default.photos,
-                title: offerData.default.title,
-                price: offerData.default.price,
-                year: offerData.default.year,
-              },
-              constructionMachinery: {
-                photos: constructionMachineryData.default.photos,
-                title: constructionMachineryData.default.title,
-                price: constructionMachineryData.default.price,
-                year: constructionMachineryData.default.year,
-              },
-              deliveryVans: {
-                photos: deliveryVansData.default.photos,
-                title: deliveryVansData.default.title,
-                price: deliveryVansData.default.price,
-                year: deliveryVansData.default.year,
-              },
-              motorcycles: {
-                photos: motorcyclesData.default.photos,
-                title: motorcyclesData.default.title,
-                price: motorcyclesData.default.price,
-                year: motorcyclesData.default.year,
-              },
-              trailers: {
-                photos: trailersData.default.photos,
-                title: trailersData.default.title,
-                price: trailersData.default.price,
-                year: trailersData.default.year,
-              },
-              trucks: {
-                photos: trucksData.default.photos,
-                title: trucksData.default.title,
-                price: trucksData.default.price,
-                year: trucksData.default.year,
-              },
-            });
-          })
-          .catch((error) => console.error("Error loading local data:", error));
+      const fetchData = async () => {
+        let data: any[] = [];
+        data[1] = await import("../testJsons/testOffer.json");
+        data[2] = await import("../testJsons/testOfferMotorcycles.json");
+        data[3] = await import("../testJsons/testOfferDeliveryVans.json");
+        data[4] = await import("../testJsons/testOfferTrucks.json");
+        data[5] = await import("../testJsons/testOfferConstructionMachinery.json");
+        data[6] = await import("../testJsons/testOfferTrailers.json");
+        data[7] = await import("../testJsons/testOfferAgriculturalMachinery.json");
+        let offerData: IOffer[] = [];
+        for (let i = 1; i < 8; i++) {
+          const { id, photos, title, price, year } = data[i].default;
+          offerData.push({ id, image: photos.length > 0 ? photos[0] : "", title, price, year });
+        }
+        setOfferData(offerData);
+      };
+      fetchData();
       }, []);
 
     return (
@@ -127,15 +41,12 @@ export default function SearchPage() {
             <ParametersInput showAllFields={true}/>
             <div className="promoted-offers">
                 <h1>Search Results</h1>
-                ?{offerData?.generalOffer.photos.map((photo) => (
-                    <OfferElement
-                        key={photo}
-                        image={photo}
-                        title={offerData?.generalOffer.title}
-                        price={offerData?.generalOffer.price}
-                        year={offerData?.generalOffer.year}
-                    />
-                ))}
+                {offerData && offerData.map((offer) => {
+                    return (
+                        <OfferElement key={offer.id} image={offer.image} title={offer.title} price={offer.price} year={offer.year} />
+                    )
+                }
+                )}
             </div>
         </div>
     )
