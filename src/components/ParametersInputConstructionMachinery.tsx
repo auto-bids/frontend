2e2ebcc137e5c0f6bf7401dc600a8ef3700ece0a
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-12-28 14:19:49 +0100

    added parameters to construction machinery search form

--- src/components/ParametersInputConstructionMachinery.tsx
+++ src/components/ParametersInputConstructionMachinery.tsx
@@ -21,10 +21,44 @@ export default function ParametersInputMainTrucks({showAllFields}: {showAllField
         setConstructionMachineryMakes(constructionMachineryDataJson);
     }, []);
 
+    const [formValues, setFormValues] = useState({
+        make: "",
+        model: "",
+        application: "",
+        yearFrom: "",
+        yearTo: "",
+        operatingHoursFrom: "",
+        operatingHoursTo: "",
+        priceFrom: "",
+        priceTo: "",
+        condition: "",
+    });
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        setFormValues({ ...formValues, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const searchPath = "/search/construction-machinery";
+        const queryParams = new URLSearchParams();
+        Object.entries(formValues).forEach(([key, value]) => {
+            if (value !== "") {
+              queryParams.append(key, value);
+            }
+        });
+        if (locationParams.position) {
+            queryParams.append("latitude", locationParams.position[0].toString());
+            queryParams.append("longitude", locationParams.position[1].toString());
+            queryParams.append("radius", locationParams.radius.toString());
+        }
+        window.location.href = `${searchPath}?${queryParams.toString()}`;
+    }
+
     return(
-        <div className="parameters-input-main">
+        <form className="parameters-input-main" onSubmit={handleSubmit}>
             <label>Make:</label>
-            <select>
+            <select name="make" value={formValues.make} onChange={handleInputChange}>
                 <option value="">Make</option>
                 {constructionMachineryMakes.map((constructionMachinery) => (
                     <option key={constructionMachinery} value={constructionMachinery}>
@@ -33,9 +67,9 @@ export default function ParametersInputMainTrucks({showAllFields}: {showAllField
                 ))}
             </select>
             <label>Model:</label>
-            <input type="text" placeholder="Model" />
+            <input type="text" placeholder="Model" name="model" value={formValues.model} onChange={handleInputChange}/>
             <label>Application:</label>
-            <select>
+            <select name="application" value={formValues.application} onChange={handleInputChange}>
                 <option value="">Application</option>
                 <option value="Articulated Dump Truck">Articulated Dump Truck</option>
                 <option value="Backhoe Loader">Backhoe Loader</option>
@@ -48,16 +82,16 @@ export default function ParametersInputMainTrucks({showAllFields}: {showAllField
             {showAllFields && (
             <>
             <label>Year:</label>
-            <input type="text" placeholder="Year from" />
-            <input type="text" placeholder="Year to" />
+            <input type="text" placeholder="Year from" name="yearFrom" value={formValues.yearFrom} onChange={handleInputChange} />
+            <input type="text" placeholder="Year to" name="yearTo" value={formValues.yearTo} onChange={handleInputChange} />
             <label>Operating Hours:</label>
-            <input type="text" placeholder="Operating Hours from" />
-            <input type="text" placeholder="Operating Hours to" />
+            <input type="text" placeholder="Operating Hours from" name="operatingHoursFrom" value={formValues.operatingHoursFrom} onChange={handleInputChange} />
+            <input type="text" placeholder="Operating Hours to" name="operatingHoursTo" value={formValues.operatingHoursTo} onChange={handleInputChange} />
             <label>Price:</label>
-            <input type="text" placeholder="Price from" />
-            <input type="text" placeholder="Price to" />
+            <input type="text" placeholder="Price from" name="priceFrom" value={formValues.priceFrom} onChange={handleInputChange} />
+            <input type="text" placeholder="Price to" name="priceTo" value={formValues.priceTo} onChange={handleInputChange} />
             <label>Condition:</label>
-            <select>
+            <select name="condition" value={formValues.condition} onChange={handleInputChange}>
                 <option value="">Condition</option>
                 <option value="New">New</option>
                 <option value="Used">Used</option>
@@ -76,6 +110,6 @@ export default function ParametersInputMainTrucks({showAllFields}: {showAllField
             <Link to="/search/construction-machinery">
                 <button>Search</button>
             </Link>
-        </div>
+        </form>
     );
 }
