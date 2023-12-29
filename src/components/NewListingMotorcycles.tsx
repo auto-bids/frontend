f4e69e0ee11f53fc41bfe4482f04582a74bda21e
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-12-29 13:37:19 +0100

    corrected new listing motorcycles form

--- src/components/NewListingMotorcycles.tsx
+++ src/components/NewListingMotorcycles.tsx
@@ -10,14 +10,7 @@ interface MakeModelMotorcycles {
 
 export default function NewListingMotorcycles() {
     const [motorcycleData, setMakeModelMotorcycles] = useState<MakeModelMotorcycles[]>([]);
-    const [selectedMake, setSelectedMake] = useState("");
-    const [selectedModel, setSelectedModel] = useState("");
     const [locationParams, setLocationParams] = useState<{ position: [number, number] | null;}>({ position: null });
-
-    const handleMakeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setSelectedMake(event.target.value);
-        setSelectedModel("");
-      };
 
     useEffect(() => {
         setMakeModelMotorcycles(makeModelMotorcycles);
@@ -27,54 +20,93 @@ export default function NewListingMotorcycles() {
         setLocationParams(params);
     };
 
+    const [formValues, setFormValues] = useState({
+        title: "",
+        make: "",
+        model: "",
+        type: "",
+        year: "",
+        mileage: "",
+        price: "",
+        description: "",
+        vinNumber: "",
+        engineCapacity: "",
+        fuelType: "",
+        power: "",
+        transmission: "",
+        drive: "",
+        registrationNumber: "",
+        firstRegistration: "",
+        condition: "",
+        location: "",
+        phoneNumber: "",
+    });
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+        const { name, value } = event.target;
+        setFormValues({
+          ...formValues,
+          [name]: value,
+        });
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(formValues);
+    };
+
+    const handleMakeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setFormValues({ ...formValues, [event.target.name]: event.target.value, model: "" });
+    };
+
     return(
         <div className='new-listing-page'>
             <h1>New Listing</h1>
-            <div className='new-listing-form'>
+            <form className='new-listing-form' name="new-listing-form" onSubmit={handleSubmit}>
                 <label>Photos:</label>
                 <input type='file' multiple accept="image/*" />
                 <label>Title:</label>
-                <input type='text' placeholder='Title' />
+                <input type='text' placeholder='Title' name="title" onChange={handleInputChange} value={formValues.title} />
                 <label>Make:</label>
-                <select value={selectedMake} onChange={handleMakeChange}>
+                <select value={formValues.make} name="make" onChange={handleMakeChange}>
                     <option value="">Make</option>
                     {motorcycleData.map((motorcycle) => (
-                    <option key={motorcycle.make} value={motorcycle.make}>
-                        {motorcycle.make}
-                    </option>
+                        <option key={motorcycle.make} value={motorcycle.make}>
+                            {motorcycle.make}
+                        </option>
                     ))}
                 </select>
                 <label>Model:</label>
-                <select value={selectedModel} onChange={(event) => setSelectedModel(event.target.value)}>
+                <select value={formValues.model} name="model" onChange={handleInputChange}>
                     <option value="">Model</option>
                     {motorcycleData
-                    .find((motorcycle) => motorcycle.make === selectedMake)?.models.map((model) => (
+                    .find((motorcycle) => motorcycle.make === formValues.make)?.models.map((model) => (
                         <option key={model} value={model}>
                         {model}
                         </option>
                     ))}
                 </select>
                 <label>Type:</label>
-                <select>
+                <select name='type' onChange={handleInputChange} value={formValues.type}>
                     <option value=''>Type</option>
                     <option value='Sportbike'>Sportbike</option>
                     <option value='Cruiser'>Cruiser</option>
                     <option value='Dirt Bike'>Dirt Bike</option>
                 </select>
                 <label>Year:</label>
-                <input type='text' placeholder='Year' />
+                <input type='text' placeholder='Year' name='year' onChange={handleInputChange} value={formValues.year} />
                 <label>Mileage:</label>
-                <input type='text' placeholder='Mileage' />
+                <input type='text' placeholder='Mileage' name='mileage' onChange={handleInputChange} value={formValues.mileage} />
                 <label>Price:</label>
-                <input type='text' placeholder='Price' />
+                <input type='text' placeholder='Price' name='price' onChange={handleInputChange} value={formValues.price} />
                 <label>Description:</label>
-                <textarea placeholder="Description" />
+                <textarea placeholder="Description" name="description" onChange={handleInputChange} value={formValues.description} />
                 <label>VIN number:</label>
-                <input type='text' placeholder='VIN number' />
+                <input type='text' placeholder='VIN number' name="vinNumber" onChange={handleInputChange} value={formValues.vinNumber} />
                 <label>Engine capacity:</label>
-                <input type='text' placeholder='Engine capacity' />
+                <input type='text' placeholder='Engine capacity' name="engineCapacity" onChange={handleInputChange} value={formValues.engineCapacity} />
                 <label>Fuel type:</label>
-                <select>
+                <select name='fuelType' onChange={handleInputChange} value={formValues.fuelType}>
                     <option value=''>Fuel type</option>
                     <option value='Petrol'>Petrol</option>
                     <option value='Diesel'>Diesel</option>
@@ -83,24 +115,23 @@ export default function NewListingMotorcycles() {
                 <label>Engine power:</label>
                 <input type='text' placeholder='Engine power' />
                 <label>Transmission:</label>
-                <select>
+                <select name="transmission" onChange={handleInputChange} value={formValues.transmission}>
                     <option value=''>Transmission</option>
                     <option value='Manual'>Manual</option>
                     <option value='Automatic'>Automatic</option>
                 </select>
-                <label>Color:</label>
                 <label>Drive type:</label>
-                <select>
+                <select name='drive' onChange={handleInputChange} value={formValues.drive}>
                     <option value=''>Drive type</option>
                     <option value='Chain'>Chain</option>
                     <option value='Belt'>Belt</option>
                     <option value='Shaft'>Shaft</option>
                 </select>
                 <label>Registration number:</label>
-                <input type='text' placeholder='Registration number' />
+                <input type='text' placeholder='Registration number' name="registrationNumber" onChange={handleInputChange} value={formValues.registrationNumber} />
                 <label>First registration:</label>
-                <input type='text' placeholder='First registration' />
-                <select>
+                <input type='text' placeholder='First registration' name="firstRegistration" onChange={handleInputChange} value={formValues.firstRegistration} />
+                <select name="condition" onChange={handleInputChange} value={formValues.condition}>
                     <option value=''>Condition</option>
                     <option value='New'>New</option>
                     <option value='Used'>Used</option>
@@ -109,9 +140,9 @@ export default function NewListingMotorcycles() {
                 <label>Location:</label>
                 <LocationInput onLocationChange={handleLocationChange} />
                 <label>Phone number:</label>
-                <input type='text' placeholder='Phone Number' />
-                <button>Submit</button>
-            </div>
+                <input type='text' placeholder='Phone Number' name="phoneNumber" onChange={handleInputChange} value={formValues.phoneNumber} />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
