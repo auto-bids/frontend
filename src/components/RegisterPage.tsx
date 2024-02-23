69b374db77ef17341b0319f3178cdeb4077c2831
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2024-02-23 11:11:36 +0100

    cleaned up register page

--- src/components/RegisterPage.tsx
+++ src/components/RegisterPage.tsx
@@ -1,48 +1,13 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-
 
 export default function RegisterPage() {
-    const navigate = useNavigate();
-
-    const onSubmit = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        event.preventDefault();
-
-        try{
-            const response = await fetch("http://localhost:4000/profiles/login/me", {
-                method: "GET",
-                credentials: "include",
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Credentials": "true",
-                },
-            });
-
-            if (response.ok){
-                console.log("great success");
-            }
-            else{
-                console.log("error");
-            }
-            navigate("/");
-
-            console.log(response.body);
-        }
-
-        catch(error){
-            console.log(error);
-        }
-    };
     
     return (
         <div>
-            <h1>Register page</h1>
-            <form>
-                <a href="http://localhost:4000/profiles/login/me">Google</a>
-                <button onClick={onSubmit}>
-                    TYLKO ARKA GDYNIA
-                </button>
-            </form>
+            <h1>Login page</h1>
+            <a href="http://localhost:4000/profiles/login/me">
+                Login with Google
+            </a>
         </div>
     );
     }
