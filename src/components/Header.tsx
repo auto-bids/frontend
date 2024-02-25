951873d8c4d3afe9706c205d2df51a240d1f00bb
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2024-02-25 18:40:05 +0100

    tailwind initialized

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,28 @@
 import React, { useEffect } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function Header(props: any) {
+  useEffect(() => {
+    // console.log("header reaload")
+  }, [props]);
 
-    useEffect(() => {
-        // console.log("header reaload")
-    },[props]);
-
-    return (
-        <div className='header'>
-            <Link to='/'>
-                <button className='home-button'>auto-bids</button>
-            </Link>
-            <Link to='/new-listing'>
-                <button className='new-listing'>New Listing</button>
-            </Link>
-            {document.cookie==="isLoggedIn=true" ?
-                <Link to='/account'>
-                    <button className='account'>Your account</button>
-                </Link>
-                :
-                <Link to='/register'>
-                    <button className='register'>Register/Login</button>
-                </Link>
-            }
-        </div>
-    )
+  return (
+    <div className='flex justify-between items-center p-4 bg-gray-800'>
+      <Link to='/'>
+        <button className='bg-blue-500 px-4 py-2 text-white rounded'>auto-bids</button>
+      </Link>
+      <Link to='/new-listing'>
+        <button className='bg-green-500 px-4 py-2 text-white rounded'>New Listing</button>
+      </Link>
+      {document.cookie === 'isLoggedIn=true' ? (
+        <Link to='/account'>
+          <button className='bg-red-500 px-4 py-2 text-white rounded'>Your account</button>
+        </Link>
+      ) : (
+        <Link to='/register'>
+          <button className='bg-purple-500 px-4 py-2 text-white rounded'>Register/Login</button>
+        </Link>
+      )}
+    </div>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: 
+  [
+    './src/**/*.{html,ts,js,tsx,ts,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
