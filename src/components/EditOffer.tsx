beea0ef5d1e2b6a1fbf7ef79f526623c48e7075b
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2024-03-10 13:55:32 +0100

    repeated code removed

--- src/components/EditOffer.tsx
+++ src/components/EditOffer.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import removePhotoFromCloudinary from '../utils/cloudinaryApi';
 
 interface IOffer {
   setEditingOfferId: (id: string) => void;
@@ -91,55 +92,6 @@ export default function EditOffer(props: IOffer) {
         formik.setFieldValue("photos", [...formik.values.photos, ""]);
     }
 
-    
-    const removePhotoFromCloudinary = async (photo: string) => {
-        const sha1 = async (string: string) => {
-            const buffer = new TextEncoder().encode(string);
-            const hash = await crypto.subtle.digest("SHA-1", buffer);
-            const hexCodes = [];
-            const view = new DataView(hash);
-            for (let i = 0; i < view.byteLength; i += 4) {
-                const value = view.getUint32(i);
-                const stringValue = value.toString(16);
-                const padding = "00000000";
-                const paddedValue = (padding + stringValue).slice(-padding.length);
-                hexCodes.push(paddedValue);
-            }
-            return hexCodes.join("");
-        };
-    
-        const timestamp = new Date().getTime();
-        const publicId = photo.split("/").slice(-1)[0].split(".")[0];
-        const apiSecret = process.env.REACT_APP_CLOUDINARY_API_SECRET;
-        const cloudName = process.env.REACT_APP_CLOUDINARY_CLOUD_NAME;
-        const apiKey = process.env.REACT_APP_CLOUDINARY_API_KEY;
-
-        const stringToSign = `public_id=${publicId}&timestamp=${timestamp}${apiSecret}`;
-        const signature = await sha1(stringToSign);
-
-        const formData = new FormData();
-        formData.append('public_id', publicId);
-        formData.append('signature', signature);
-        if (apiKey) {
-            formData.append('api_key', apiKey);
-        }
-        formData.append('timestamp', timestamp.toString());
-        try {
-            const response = await fetch(`${cloudName}/image/destroy`, {
-                method: 'POST',
-                body: formData,
-            });
-
-            if (response.ok) {
-                const data = await response.json();
-                console.log(data);
-            } else {
-                console.log('Error deleting image');
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    }
 
     const handleRemovePhoto = (index: number) => {
         const photos = formik.values.photos.filter((photo, i) => i !== index);
