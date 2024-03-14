393bcf8a627f0dd2a6822113a640e442f62fe36f
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2024-03-14 18:24:11 +0100

    changed admin page

--- src/components/Admin/AdminPage.tsx
+++ src/components/Admin/AdminPage.tsx
@@ -1,123 +1,53 @@
 import React, {useState, useEffect} from "react";
 
-interface IProfile {
-    email: string;
-    name: string;
-    surname: string;
-    profilePicture: string;
-}
-
-interface IOffer {
-    agriculturalMachinery: {
-        photos: string[];
-        title: string;
-        price: number;
-        year: number;
-    };
-    generalOffer: {
-        photos: string[];
-        title: string;
-        price: number;
-        year: number;
-    };
-    constructionMachinery: {
-        photos: string[];
-        title: string;
-        price: number;
-        year: number;
-    };
-    deliveryVans: {
-        photos: string[];
-        title: string;
-        price: number;
-        year: number;
-    };
-    motorcycles: {
-        photos: string[];
-        title: string;
-        price: number;
-        year: number;
-    };
-    trailers: {
-        photos: string[];
-        title: string;
-        price: number;
-        year: number;
-    };
-    trucks: {
-        photos: string[];
-        title: string;
-        price: number;
-        year: number;
-    };
-};
-
 export default function AdminPage() {
-
-    const [profiles, setProfiles] = useState<IProfile[]>([]);
-    const [offers, setOffers] = useState<IOffer[]>([]);
-
-
-    useEffect(() => {
-        fetch("http://localhost:3000/profiles")
-            .then((response) => response.json())
-            .then((data) => setProfiles(data));
-    }, []);
-
-    useEffect(() => {
-        fetch("http://localhost:3000/offers")
-            .then((response) => response.json())
-            .then((data) => setOffers(data));
-    }, []);
-
-    const banProfile = (profile: IProfile) => {
-        fetch("http://localhost:3000/profiles", {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(profile),
-        })
-            .then((response) => response.json())
-            .then((data) => setProfiles(data));
-    };
-
-    const banOffer = (offer: IOffer) => {
-        fetch("http://localhost:3000/offers", {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(offer),
-        })
-            .then((response) => response.json())
-            .then((data) => setOffers(data));
-    }
-
-    return (
-        <div className="admin-page">
-            <h1>Admin Page</h1>
-            <div className="admin-page-profiles">
-                {profiles.map((profile) => (
-                    <div className="admin-page-profile">
-                        <img src={profile.profilePicture} alt="profile picture" />
-                        <p>{profile.name} {profile.surname}</p>
-                        <p>{profile.email}</p>
-                        <button onClick={() => banProfile(profile)}>Ban</button>
-                    </div>
-                ))}
+    const [category, setCategory] = useState("users");
+    const [usersEmail, setUsersEmail] = useState("");
+    const [offerID, setOfferID] = useState("");
+    const [usersReason, setUsersReason] = useState("");
+    const [offersReason, setOffersReason] = useState("");
+    
+    return(
+        <div className="h-screen">
+            <div className="page-buttons flex justify-center items-center h-1/6 space-x-4">
+                <button className="bg-teal-500 hover:bg-teal-700 text-white py-2 px-4 rounded transition duration-300" onClick={() => setCategory("users")}>
+                    Users
+                </button>
+                <button className="bg-teal-500 hover:bg-teal-700 text-white py-2 px-4 rounded transition duration-300" onClick={() => setCategory("offers")}>
+                    Offers
+                </button>
+                <button className="bg-teal-500 hover:bg-teal-700 text-white py-2 px-4 rounded transition duration-300" onClick={() => setCategory("statistics")}>
+                    Statistics
+                </button>
             </div>
-            <div className="admin-page-offers">
-                {offers.map((offer) => (
-                    <div className="admin-page-offer">
-                        <img src={offer.agriculturalMachinery.photos[0]} alt="offer picture" />
-                        <p>{offer.agriculturalMachinery.title}</p>
-                        <p>{offer.agriculturalMachinery.price}</p>
-                        <p>{offer.agriculturalMachinery.year}</p>
-                        <button onClick={() => banOffer(offer)}>Ban</button>
+            <div className="page-content h-5/6">
+                {category === "users" && (
+                    <div className="flex flex-col items-center justify-center h-full space-y-4 p-4">
+                        <h1 className="text-2xl font-bold">Ban user</h1>
+                        <input type="text" placeholder="User's email" className="w-full h-12 px-4 border border-gray-300 rounded" onChange={(e) => setUsersEmail(e.target.value)} />
+                        <textarea placeholder="Reason for ban" className="w-full h-32 px-4 border border-gray-300 rounded" onChange={(e) => setUsersReason(e.target.value)}/>
+                        <button className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded transition duration-300">
+                            Ban
+                        </button>
                     </div>
-                ))}
+                )}
+                {category === "offers" && (
+                    <div className="flex flex-col items-center justify-center h-full space-y-4 p-4">
+                        <h1 className="text-2xl font-bold">Ban offer</h1>
+                        <input type="text" placeholder="Offer ID" className="w-full h-12 px-4 border border-gray-300 rounded" />
+                        <textarea placeholder="Reason for ban" className="w-full h-32 px-4 border border-gray-300 rounded"></textarea>
+                        <button className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded transition duration-300">
+                            Ban
+                        </button>
+                    </div>
+                )}
+                {category === "statistics" && (
+                    <div>
+                        <h1>Statistics</h1>
+
+                    </div>
+                )}
             </div>
         </div>
-    );
+    )
 }
